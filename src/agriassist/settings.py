22c3c137df9f2e5ffd577ec36df4b9f5238f2099
manogna25 <sivamanognalanka@gmail.com>
2024-10-28 11:19:16 +0530

    UpdatedHomePage

--- src/agriassist/settings.py
+++ src/agriassist/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "AgriAssist",
         "USER": "postgres",
-        "PASSWORD": "Srujan@123098",
+        "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
