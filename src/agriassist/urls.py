fb4de9626a136f1e35cb4178a61aafa2270897a9
manogna25 <sivamanognalanka@gmail.com>
2024-12-01 20:11:22 +0530

    Added About us and Contact us Pages

--- src/agriassist/urls.py
+++ src/agriassist/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 urlpatterns = [
     path("", include("login.urls")),
     path("shop/", include("shop.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
